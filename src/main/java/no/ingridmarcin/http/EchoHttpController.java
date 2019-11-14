1d2e2a00cc9348c3b4d8ae532317db231c188dbe
Ingrid-Alice Bløtekjær <you@example.com>
2019-11-14 12:47:53 +0100

    adds EchoHttpController

--- src/main/java/no/ingridmarcin/http/EchoHttpController.java
+++ src/main/java/no/ingridmarcin/http/EchoHttpController.java
@@ -1,4 +1,28 @@
 package no.ingridmarcin.http;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
 class EchoHttpController implements HttpController {
+
+    @Override
+    public void handle(String requestAction, String path, Map<String, String> queryParameters, String body, OutputStream outputStream) throws IOException {
+        if (requestAction.equals("POST")) {
+            queryParameters = HttpServer.parseQueryString(body);
+        }
+
+        String status = queryParameters.getOrDefault("Status", "200");
+        String location = queryParameters.getOrDefault("Location", null);
+        String responseBody = queryParameters.getOrDefault("Body", "Hello World");
+        int contentLength = responseBody.length();
+        outputStream.write(("HTTP/1.1 " + status + " OK\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: " + contentLength + "\r\n" +
+                "Connection: close\r\n" +
+                (location != null ? "Location: " + location + "\r\n" : "") +
+                "\r\n" +
+                responseBody).getBytes());
+    }
+
 }
