129decc82746a612ccfbb2a6dce28eec654ad7ea
amish1188 <martin.joodd@gmail.com>
2019-11-15 12:46:06 +0100

    adds Task and TaskDao class

--- src/main/java/no/ingridmarcin/taskmanager/Task.java
+++ src/main/java/no/ingridmarcin/taskmanager/Task.java
@@ -1,7 +1,50 @@
 package no.ingridmarcin.taskmanager;
 
 
+import java.util.Objects;
+
 public class Task {
+    private long id;
+    private String taskName;
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Task task = (Task) o;
+        return id == task.id &&
+                Objects.equals(id, task.id) &&
+                Objects.equals(taskName, task.taskName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, taskName);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id='" + id + '\'' +
+                ", taskName=" + taskName +
+                '}';
+    }
 }
